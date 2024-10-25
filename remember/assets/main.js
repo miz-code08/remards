c89c45375414c09fa4f550f8c9851493236d7c4b
miz <buiductai357@gmail.com>
2024-10-25 14:50:39 +0700

    test 2

--- remember/assets/main.js
+++ remember/assets/main.js
@@ -169,5 +169,5 @@ window.onload = function() {
     window.addEventListener('beforeunload', function (event) {
         event.preventDefault();
         event.returnValue = '';
-    });    
+    });
 };
